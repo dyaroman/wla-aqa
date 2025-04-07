9d3fe84d04d5f7037b4ac0a69ec864029e1f6b34
Roman Diachenko <dyaroman@gmail.com>
2025-04-07 11:23:23 +0300

    refactoring TestResultItem component

--- src/components/TestResultItem.vue
+++ src/components/TestResultItem.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps({
+const props = defineProps({
   testResult: {
     type: Object,
     required: true,
@@ -13,17 +13,18 @@ defineProps({
     required: true,
   },
 });
+
+const { testResult, buildNumber } = props;
+const errorMessage = testResult.err?.message?.replaceAll('\\', '');
+const src = `https://dyaroman.github.io/wla-e2e/data/${buildNumber ? `${buildNumber}/` : ''}images/${testResult.img}`;
 </script>
 
 <template>
   <details v-if="category === 'fail'">
     <summary>{{ testResult.title }}</summary>
     <div class="error-message" v-if="testResult.fail">
-      <img
-        :src="`https://dyaroman.github.io/wla-e2e/data/${buildNumber ? buildNumber + '/' : ''}images/${testResult.img}`"
-        :alt="testResult.err?.message?.replaceAll('\\', '')"
-      />
-      {{ testResult.err?.message?.replaceAll('\\', '') }}
+      <img :src :alt="errorMessage" />
+      {{ errorMessage }}
     </div>
   </details>
   <template v-else>
