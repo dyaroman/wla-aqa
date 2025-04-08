635eb8bae8dbb8407bf90d6facc727afed12b4b3
Roman Diachenko <dyaroman@gmail.com>
2025-04-08 09:29:36 +0300

    load screenshot after user click

--- src/components/TestResultItem.vue
+++ src/components/TestResultItem.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { ref } from 'vue';
+
+import Loader from '@/components/Loader.vue';
 import { reformatErrorMessage } from '@/misc/helpers.js';
 
 const { buildNumber, category, testResult } = defineProps({
@@ -19,23 +22,38 @@ const { buildNumber, category, testResult } = defineProps({
 const errorMessage =
   category === 'fail' ? reformatErrorMessage(testResult.err?.message) : null;
 
-const src =
-  category === 'fail'
-    ? `https://dyaroman.github.io/wla-e2e/data/${buildNumber ? `${buildNumber}/` : ''}images/${testResult.img}`
-    : null;
+const screenshotSrc = ref(null);
+const isScreenshotLoading = ref(false);
+
+const startScreenshotLoading = () => {
+  if (!screenshotSrc.value) {
+    screenshotSrc.value = `https://dyaroman.github.io/wla-e2e/data/${buildNumber ? `${buildNumber}/` : ''}images/${testResult.img}`;
+    isScreenshotLoading.value = true;
+  }
+};
 </script>
 
 <template>
   <details v-if="category === 'fail'">
     <summary>{{ testResult.title }}</summary>
     <div class="error-message" v-if="testResult.fail">
-      <img :src :alt="errorMessage" />
-      <pre v-if="errorMessage.includes('{')"
-        >{{ errorMessage }}
-      </pre>
+      <pre v-if="errorMessage.includes('{')">{{ errorMessage }}</pre>
       <template v-else>
         {{ errorMessage?.replaceAll('\\', '') }}
       </template>
+      <details>
+        <summary @click="startScreenshotLoading">Screenshot</summary>
+        <div class="screenshot">
+          <Loader v-if="isScreenshotLoading" />
+          <img
+            v-show="!isScreenshotLoading && screenshotSrc"
+            :src="screenshotSrc"
+            alt="Failed to load screenshot"
+            @load="isScreenshotLoading = false"
+            @error="isScreenshotLoading = false"
+          />
+        </div>
+      </details>
     </div>
   </details>
   <template v-else>
