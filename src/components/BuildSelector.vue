61e23907bc7e9306fb973aa888e1ecb6c965c269
Roman Diachenko <dyaroman@gmail.com>
2025-04-09 16:23:17 +0300

    refactoring

--- src/components/BuildSelector.vue
+++ src/components/BuildSelector.vue
@@ -14,15 +14,15 @@ defineProps({
 
 const emit = defineEmits(['update:modelValue']);
 
-const emitValue = (event) => {
+function updateValue(event) {
   emit('update:modelValue', event.target.value);
-};
+}
 </script>
 
 <template>
   <section>
     <h4>Select build:</h4>
-    <select class="select" :value="modelValue" @change="emitValue">
+    <select class="select" :value="modelValue" @change="updateValue">
       <option value="" disabled>Select build</option>
       <option v-for="build in builds" :key="build.number" :value="build.number">
         {{ reformatBuildNumber(build.number) }} - {{ timeAgo(build.timestamp) }}
