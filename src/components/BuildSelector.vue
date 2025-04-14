675a66409ff35ce69ceae3cbd1513ea0d9a63977
Roman Diachenko <dyaroman@gmail.com>
2025-04-14 17:08:00 +0300

    Group build options by date in selector dropdown

--- src/components/BuildSelector.vue
+++ src/components/BuildSelector.vue
@@ -1,7 +1,9 @@
 <script setup>
+import { computed } from 'vue';
+
 import { reformatBuildNumber, timeAgo } from '@/misc/helpers.js';
 
-defineProps({
+const { builds } = defineProps({
   builds: {
     type: Array,
     required: true,
@@ -17,9 +19,23 @@ defineProps({
 });
 
 const emit = defineEmits(['update:modelValue']);
+const groupedBuilds = computed(() => groupItemsByDate(builds));
 
 function updateValue(event) {
   emit('update:modelValue', event.target.value);
+}
+
+function groupItemsByDate(arr) {
+  const groups = {};
+  for (const item of arr) {
+    const date = item.timestamp.split(' ')[1].split('/'); // 04/14/2025
+    const group = `${date[2]}/${date[0]}/${date[1]}`; // 2025/04/14
+    if (!groups[group]) {
+      groups[group] = [];
+    }
+    groups[group].push(item);
+  }
+  return groups;
 }
 </script>
 
@@ -33,9 +49,20 @@ function updateValue(event) {
       :disabled="disabled"
     >
       <option value="" disabled>Select build</option>
-      <option v-for="build in builds" :key="build.number" :value="build.number">
-        {{ reformatBuildNumber(build.number) }} - {{ timeAgo(build.timestamp) }}
-      </option>
+      <optgroup
+        v-for="(group, date) in groupedBuilds"
+        :key="date"
+        :label="date"
+      >
+        <option
+          v-for="build in group"
+          :key="build.number"
+          :value="build.number"
+        >
+          {{ reformatBuildNumber(build.number) }} -
+          {{ timeAgo(build.timestamp) }}
+        </option>
+      </optgroup>
     </select>
   </section>
 </template>
