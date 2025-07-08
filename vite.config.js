0996afb33b8768801a696bf840b9ff78d07a8f70
Roman Diachenko <dyaroman@gmail.com>
2025-07-08 09:55:18 +0300

    remove unused jsx plugin

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import { execSync } from 'child_process';
 
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [vue(), vueJsx(), vueDevTools()],
+    plugins: [vue(), vueDevTools()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
